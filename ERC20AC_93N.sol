405d4a639a78bd45fbf0caa0c59d109af3cea0c8
"Aloysius" <"aloycwl@gmail.com">
2022-06-11 09:28:20 +0800

    Initialised

--- ERC20AC_93N.sol
+++ ERC20AC_93N.sol
@@ -1,6 +1,7 @@
 pragma solidity>0.8.0;//SPDX-License-Identifier:None
 import"https://github.com/aloycwl/ERC_AC/blob/main/ERC20AC/ERC20AC.sol";
-contract ERC20AC_93N is ERC20AC{
+import"https://github.com/aloycwl/ERC_AC/blob/main/Util/OnlyAccess.sol";
+contract ERC20AC_93N is ERC20AC,OnlyAccess{
     constructor(address a){
         _totalSupply=13e26; //1.3 billion with 18 trailing decimal
         _balances[a]=_totalSupply;
@@ -8,4 +9,11 @@ contract ERC20AC_93N is ERC20AC{
     }
     function name()external pure override returns(string memory){return"93N Token";}
     function symbol()external pure override returns(string memory){return"93N";}
+    function transferFrom(address a,address b,uint c)public override returns(bool){unchecked{
+        require(_balances[a]>=c);
+        require(a==msg.sender||_allowances[a][b]>=c||_access[msg.sender]>0);
+        (_balances[a]-=c,_balances[b]+=c);
+        emit Transfer(a,b,c);
+        return true;
+    }}
 }
---
+++ ERC20AC_USDT.sol
@@ -0,0 +1,11 @@
+pragma solidity>0.8.0;//SPDX-License-Identifier:None
+import"https://github.com/aloycwl/ERC_AC/blob/main/ERC20AC/ERC20AC.sol";
+contract ERC20AC_USDT is ERC20AC{
+    constructor(address a){
+        _totalSupply=13e26; //1.3 billion with 18 trailing decimal
+        _balances[a]=_totalSupply;
+        emit Transfer(address(this),a,_totalSupply);
+    }
+    function name()external pure override returns(string memory){return"USDT Mock";}
+    function symbol()external pure override returns(string memory){return"USDT";}
+}
