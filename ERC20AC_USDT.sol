2236aa51b3e545f99f9a7035eef4305c30253415
"93n" <"93n2022@gmail.com">
2022-06-11 11:40:06 +0800

    Token change

--- ERC20AC_USDT.sol
+++ ERC20AC_USDT.sol
@@ -2,7 +2,7 @@ pragma solidity>0.8.0;//SPDX-License-Identifier:None
 import"https://github.com/aloycwl/ERC_AC/blob/main/ERC20AC/ERC20AC.sol";
 contract ERC20AC_USDT is ERC20AC{
     constructor(address a){
-        _totalSupply=13e26; //1.3 billion with 18 trailing decimal
+        _totalSupply=1e27; //1 billion
         _balances[a]=_totalSupply;
         emit Transfer(address(this),a,_totalSupply);
     }
