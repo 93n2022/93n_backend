3561813cb86d2d97bc4a7e4092d50ad05d7cd41f
"93n2022" <"93n2022@gmail.com">
2022-06-24 15:57:23 +0800

    set access

--- Swap_93N.sol
+++ Swap_93N.sol
@@ -1,6 +1,8 @@
 pragma solidity>0.8.0;//SPDX-License-Identifier:None
 contract Swap_93N{
+    uint public fee;
     address private _owner;
+    mapping(address=>mapping(address=>uint[2]))public pairs;
     constructor(){
         _owner=msg.sender;
     }
@@ -8,5 +10,14 @@ contract Swap_93N{
         c=new uint[](2);
         c[0]=a/10;
     }
-    
+    function addLiqudity(address conAddr1,address conAddr2,uint amount1,uint amount2)external{unchecked{
+        require(amount1>0);
+        require(amount2>0);
+        (pairs[conAddr1][conAddr2][0]+=amount1,pairs[conAddr1][conAddr2][1]+=amount2,
+        pairs[conAddr2][conAddr1][0]+=amount2,pairs[conAddr2][conAddr1][1]+=amount1);
+    }}
+    function setFee(uint percent)external{
+        require(_owner==msg.sender);
+        fee=percent;
+    }
 }
