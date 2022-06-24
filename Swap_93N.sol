0ade6d838da2a02505c07624692bf1e529b145de
"93n2022" <"93n2022@gmail.com">
2022-06-24 16:35:36 +0800

    removeLiquidity

--- Swap_93N.sol
+++ Swap_93N.sol
@@ -7,26 +7,25 @@ contract Swap_93N{
     constructor(){
         _owner=msg.sender;
     }
-    function getAmountsOut(uint a,address[]memory)external pure returns(uint[]memory c){
-        c=new uint[](2);
-        c[0]=a/10;
+    modifier OnlyOwner(){
+        require(_owner==msg.sender);_;
     }
-    function addLiqudity(address conAddr1,address conAddr2,uint amount1,uint amount2)external{unchecked{
-        require(amount1>0);
-        require(amount2>0);
-        require(_owner==msg.sender);
+    function addLiqudity(address conAddr1,address conAddr2,uint amount1,uint amount2)external OnlyOwner{unchecked{
         IERC20(conAddr1).transferFrom(msg.sender,address(this),amount1);
         IERC20(conAddr2).transferFrom(msg.sender,address(this),amount2);
         (pairs[conAddr1][conAddr2][0]+=amount1,pairs[conAddr1][conAddr2][1]+=amount2,
         pairs[conAddr2][conAddr1][0]+=amount2,pairs[conAddr2][conAddr1][1]+=amount1);
     }}
-    function setFee(uint percent)external{
+    function RemoveLiqudity(address conAddr1,address conAddr2,uint amount1,uint amount2)external OnlyOwner{unchecked{
         require(_owner==msg.sender);
+        IERC20(conAddr1).transferFrom(address(this),msg.sender,amount1);
+        IERC20(conAddr2).transferFrom(address(this),msg.sender,amount2);
+        (pairs[conAddr1][conAddr2][0]-=amount1,pairs[conAddr1][conAddr2][1]-=amount2,
+        pairs[conAddr2][conAddr1][0]-=amount2,pairs[conAddr2][conAddr1][1]-=amount1);
+    }}
+    function setFee(uint percent)external OnlyOwner{
         fee=percent;
     }
-    function getPrice(address conAddr1,address conAddr2,uint amount)public view returns(uint){{
-        return amount/pairs[conAddr1][conAddr2][0]*pairs[conAddr1][conAddr2][1];
-    }}
     function exchange(address conAddr1,address conAddr2,uint amount1)external{unchecked{
         uint amount2=getPrice(conAddr1,conAddr2,amount1);
         require(amount2>0);
@@ -35,5 +34,9 @@ contract Swap_93N{
         IERC20(conAddr2).transferFrom(address(this),msg.sender,amount2);
         (pairs[conAddr1][conAddr2][0]+=amount1,pairs[conAddr1][conAddr2][1]-=amount2,
         pairs[conAddr2][conAddr1][0]-=amount2,pairs[conAddr2][conAddr1][1]+=amount1);
+    }}
+
+    function getPrice(address conAddr1,address conAddr2,uint amount)public view returns(uint){{
+        return amount/pairs[conAddr1][conAddr2][0]*pairs[conAddr1][conAddr2][1];
     }}
 }
