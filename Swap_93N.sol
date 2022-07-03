f6e1bd876421d1e5bed83e84b2e229135f2337c0
"Aloysius" <"aloycwl@gmail.com">
2022-07-03 17:45:52 +0800

    1

--- Swap_93N.sol
+++ Swap_93N.sol
@@ -25,26 +25,19 @@ contract Swap_93N{
     function setFee(uint percent)external OnlyOwner{
         fee=percent;
     }
-    function exchange(address conAddr1,address conAddr2,uint amount1)external payable{unchecked{
-        uint amount2=getPrice(conAddr1,conAddr2,amount1);
+    function exchange(uint amount1,address[]memory a)external payable{unchecked{
+        uint amount2=AmountOut(amount1,a);
         require(amount2>0);
-        require(amount2<=pairs[conAddr2][conAddr1]);
-        IERC20(conAddr1).transferFrom(msg.sender,address(this),amount1);
-        IERC20(conAddr2).transferFrom(address(this),msg.sender,amount2);
-        (pairs[conAddr1][conAddr2]+=amount1,pairs[conAddr2][conAddr1]-=amount2);
-    }}
-    function getPrice(address conAddr1,address conAddr2,uint amount)public view returns(uint a){{
-        /*
-        Deduct the fees first
-        If any amount exceeded the available amount return 0
-        */
-        a=pairs[conAddr1][conAddr2]*amount/pairs[conAddr1][conAddr2]*fee/10000;
-        if(a>pairs[conAddr1][conAddr2]||amount>pairs[conAddr1][conAddr2])a=0;
+        require(amount2<=pairs[a[1]][a[0]]);
+        IERC20(a[0]).transferFrom(msg.sender,address(this),amount1);
+        IERC20(a[1]).transferFrom(address(this),msg.sender,amount2);
+        (pairs[a[0]][a[1]]+=amount1,pairs[a[1]][a[0]]-=amount2);
     }}
     function AmountOut(uint amt,address[]memory a)public view returns(uint amt2){{
         (uint d,uint _L1,uint _L2)=(amt%_decimal,pairs[a[0]][a[1]],pairs[a[1]][a[0]]);
         (amt-=d,amt/=_decimal);
         for(uint i=0;i<amt;i++)(_L2+=_L2*_decimal/_L1,_L1-=_decimal);
-        amt2=_L2*fee/1e4+d>0?pairs[a[0]][a[1]]*d/pairs[a[1]][a[0]]*fee/1e4*d:0;
+        amt2=(_L2-pairs[a[1]][a[0]])*fee/1e4+d>0?pairs[a[0]][a[1]]*d/pairs[a[1]][a[0]]*fee/1e4*d:0;
     }}
 }
+//1000000000000000000
