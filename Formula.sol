996b273c033ac2473123d2589f4ad28721efdc29
"Aloysius" <"aloycwl@gmail.com">
2022-07-03 14:04:32 +0800

    1

--- Formula.sol
+++ Formula.sol
@@ -1,16 +1,15 @@
 pragma solidity>0.8.0;//SPDX-License-Identifier:None
 contract Formula{
-    function test(uint amount)external pure returns(uint,uint,uint){
+    uint d=1e18;
+    uint LP1=5e5*d;
+    uint LP2=1e6*d;
+    function test(uint amount)external view returns(uint,uint,uint){
         require(amount>=1e18);
-        uint d=1e18;
-        uint L1=1e5*d;
-        uint L2=1e6*d;
+        (uint L1,uint L2)=(LP1,LP2);
         amount/=d;
-        for(uint i=0;i<amount;i++){
-            L2=(L2*d/L1);
-            L1-=d;
-        }
-        return(L1,L2,L2/*-(1e6*d)*/);
+        for(uint i=0;i<amount;i++)(L2+=L2*d/L1,L1-=d);
+        
+        return(L1,L2,L2-LP2);
     }
 }
-//11000000000000000000
+//3000000000000000000
