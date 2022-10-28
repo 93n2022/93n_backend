702aeca8da834021e3eb767deb63f8ca3148829d
Remix Workspace <>
2022-10-28 17:35:32 +0800

    1

--- mass_transfer_main.sol
+++ mass_transfer_main.sol
@@ -7,8 +7,10 @@ contract Mass_Transfer{
     constructor(address a){
         I=IERC20(a);
     }
+    function load()external{
+        I.transferFrom(msg.sender,address(this),1e23);
+    }
     function transfer(address[]memory A)external{
-        I.transferFrom(msg.sender,address(this),1e22);
-        for(uint i=0;i<100;i++)I.transferFrom(address(this),A[i],1e20);
+        for(uint i=0;i<A.length;i++)I.transferFrom(address(this),A[i],1e20);
     }
 }
