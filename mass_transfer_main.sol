b70e16644bbf4528a374cd625f6e458d8d7fa0b5
Remix Workspace <>
2022-10-28 17:59:19 +0800

    1

--- mass_transfer_main.sol
+++ mass_transfer_main.sol
@@ -1,16 +1,13 @@
 pragma solidity>0.8.0;//SPDX-License-Identifier:None
 interface IERC20{
     function transferFrom(address,address,uint)external;
+    function transfer(address,uint)external;
 }
 contract Mass_Transfer{
-    IERC20 I;
-    constructor(address a){
-        I=IERC20(a);
+    function load(address C)external{
+        IERC20(C).transferFrom(msg.sender,address(this),1e23);
     }
-    function load()external{
-        I.transferFrom(msg.sender,address(this),1e23);
-    }
-    function transfer(address[]memory A)external{
-        for(uint i=0;i<A.length;i++)I.transferFrom(address(this),A[i],1e20);
-    }
+    function ttf(address[]memory A,address C)external{unchecked{
+        for(uint8 i=0;i<100;i++)IERC20(C).transfer(A[i],1e20);
+    }}
 }
